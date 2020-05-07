6b1953e6b9a0594734dbaf0395ef4509f83dea4b
Abdul Hafeez <abdulhafeezmvij001@gmail.com>
2020-05-07 11:56:08 +0530

    Commiting Changes.

--- src/com/timbuchalka/StockList.java
+++ src/com/timbuchalka/StockList.java
@@ -20,6 +20,10 @@ public class StockList {
     public StockList() {
         this.list = new LinkedHashMap<>();
     }
+    
+    public void my Method() {
+    	
+    }
 
     public int addStock(StockItem item) {
         if(item != null) {
